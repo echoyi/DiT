696c603a5222f5f116f206b7ec2eeeb8826ab229
Yi Sui <amy@layer6.ai>
2023-03-13 23:16:31 -0400

    modification to generate equal amount of samples for every class

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@
 """
 Sample new images from a pre-trained DiT.
 """
+import os
 import torch
 torch.backends.cuda.matmul.allow_tf32 = True
 torch.backends.cudnn.allow_tf32 = True
@@ -16,6 +17,7 @@ from diffusers.models import AutoencoderKL
 from download import find_model
 from models import DiT_models
 import argparse
+from PIL import Image
 
 
 def main(args):
@@ -44,29 +46,53 @@ def main(args):
     vae = AutoencoderKL.from_pretrained(f"stabilityai/sd-vae-ft-{args.vae}").to(device)
 
     # Labels to condition the model with (feel free to change):
-    class_labels = [207, 360, 387, 974, 88, 979, 417, 279]
+    img_id = args.start_img_id
+    class_labels_all = [i for i in range(args.num_classes)]*args.n_iter
+    
+    bs = 10
+    if len(class_labels_all)%bs != 0: raise ValueError
+    for iter_idx in range(len(class_labels_all)//bs):
+        class_labels = class_labels_all[iter_idx*bs: (iter_idx+1)*bs]
+        class_id = 0
+        n = len(class_labels)
+        z = torch.randn(n, 4, latent_size, latent_size, device=device)
+        y = torch.tensor(class_labels, device=device)
 
-    # Create sampling noise:
-    n = len(class_labels)
-    z = torch.randn(n, 4, latent_size, latent_size, device=device)
-    y = torch.tensor(class_labels, device=device)
+        # Setup classifier-free guidance:
+        z = torch.cat([z, z], 0)
+        y_null = torch.tensor([1000] * n, device=device)
+        y = torch.cat([y, y_null], 0)
+        model_kwargs = dict(y=y, cfg_scale=args.cfg_scale)
 
-    # Setup classifier-free guidance:
-    z = torch.cat([z, z], 0)
-    y_null = torch.tensor([1000] * n, device=device)
-    y = torch.cat([y, y_null], 0)
-    model_kwargs = dict(y=y, cfg_scale=args.cfg_scale)
+        # Sample images:
+        samples = diffusion.p_sample_loop(
+            model.forward_with_cfg, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
+        )
+        samples, _ = samples.chunk(2, dim=0)  # Remove null class samples
+        samples = vae.decode(samples / 0.18215).sample
 
-    # Sample images:
-    samples = diffusion.p_sample_loop(
-        model.forward_with_cfg, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
-    )
-    samples, _ = samples.chunk(2, dim=0)  # Remove null class samples
-    samples = vae.decode(samples / 0.18215).sample
+        # Save and display images:
+        # save_image(samples, f"sample_{iter_idx}.png", nrow=4, normalize=True, value_range=(-1, 1))
+        for sample in samples:
+            img_np = norm_range(sample.clone(), value_range=(-1, 1)).mul(255).\
+                        add_(0.5).clamp_(0, 255).permute(1, 2, 0).\
+                        to("cpu", torch.uint8).numpy()
+            im = Image.fromarray(img_np)
+            if not os.path.exists(f'res/{class_labels[class_id]}'): os.makedirs(f'res/{class_labels[class_id]}')
+            im.save(f'res/{class_labels[class_id]}/{img_id}.png')
+            class_id += 1
+            img_id += 1
 
-    # Save and display images:
-    save_image(samples, "sample.png", nrow=4, normalize=True, value_range=(-1, 1))
+def norm_ip(img, low, high):
+            img.clamp_(min=low, max=high)
+            img.sub_(low).div_(max(high - low, 1e-5))
 
+def norm_range(t, value_range):
+    if value_range is not None:
+        norm_ip(t, value_range[0], value_range[1])
+    else:
+        norm_ip(t, float(t.min()), float(t.max()))
+    return t
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -77,6 +103,8 @@ if __name__ == "__main__":
     parser.add_argument("--cfg-scale", type=float, default=4.0)
     parser.add_argument("--num-sampling-steps", type=int, default=250)
     parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--n_iter", type=int, default=100, help='number of samples per class, default 100')
+    parser.add_argument("--start_img_id", type=int, default=0, help='img id of the first generated image, default 0')
     parser.add_argument("--ckpt", type=str, default=None,
                         help="Optional path to a DiT checkpoint (default: auto-download a pre-trained DiT-XL/2 model).")
     args = parser.parse_args()
