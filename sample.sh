56042521cbe3666ae70df20cc8f3425bdee98d24
Yi Sui <amy@layer6.ai>
2023-03-14 21:32:23 -0400

    update script

--- sample.sh
+++ sample.sh
@@ -1,13 +1,24 @@
 # conda env create -f environment.yml
 # conda activate DiT
-python sample.py --image-size 256 --seed 1 --n_iter 10 --start_img_id 0 --bs 10 #Amy
+# python sample.py --image-size 256 --seed 1 --n_iter 10 --start_img_id 0 --bs 10 #Amy
+# zip -r DiT-XL-2-256-0-9999 res/
 
 # python sample.py --image-size 256 --seed 2 --n_iter 10 --start_img_id 10000 --bs 10 #jesse
+# zip -r DiT-XL-2-256-10000-19999 res/
 # python sample.py --image-size 256 --seed 3 --n_iter 10 --start_img_id 20000 --bs 10 #zhaoyan/satya [thanks satya]
-# python sample.py --image-size 256 --seed 4 --n_iter 10 --start_img_id 30000 --bs 10
+# zip -r DiT-XL-2-256-20000-29999 res/
+
+python sample.py --image-size 256 --seed 4 --n_iter 10 --start_img_id 30000 --bs 10 #Amy
+zip -r DiT-XL-2-256-30000-39999 res/
 # python sample.py --image-size 256 --seed 5 --n_iter 10 --start_img_id 40000 --bs 10
+# zip -r DiT-XL-2-256-40000-49999 res/
 # python sample.py --image-size 256 --seed 6 --n_iter 10 --start_img_id 50000 --bs 10
+# zip -r DiT-XL-2-256-50000-549999 res/
 # python sample.py --image-size 256 --seed 7 --n_iter 10 --start_img_id 60000 --bs 10
+# zip -r DiT-XL-2-256-60000-69999 res/
 # python sample.py --image-size 256 --seed 8 --n_iter 10 --start_img_id 70000 --bs 10
+# zip -r DiT-XL-2-256-70000-79999 res/
 # python sample.py --image-size 256 --seed 9 --n_iter 10 --start_img_id 80000 --bs 10
+# zip -r DiT-XL-2-256-80000-89999 res/
 # python sample.py --image-size 256 --seed 10 --n_iter 10 --start_img_id 90000 --bs 10
+# zip -r DiT-XL-2-256-90000-99999 res/
