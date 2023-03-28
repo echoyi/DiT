9280212e41e2257b5928b6da3bb7cd2c6fe77431
Yi(Amy) Sui <suiyiamy@gmail.com>
2023-03-28 09:26:18 -0400

    Update sample.sh
    remove old folder before starting

--- sample.sh
+++ sample.sh
@@ -1,31 +1,43 @@
 # conda env create -f environment.yml
 # conda activate DiT
-# python sample.py --image-size 256 --seed 1 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+
+
+# rm -rf res/
+# python sample.py --image-size 256 --seed 1 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-0-9999 res/
 
-# python sample.py --image-size 256 --seed 2 ---n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 2 ---n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-10000-19999 res/
 
-# python sample.py --image-size 256 --seed 3 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 3 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-20000-29999 res/
 
-# python sample.py --image-size 256 --seed 4 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250 #brendan
+# rm -rf res/
+# python sample.py --image-size 256 --seed 4 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250 #brendan
 # zip -r DiT-XL-2-256-30000-39999 res/
 
-# python sample.py --image-size 256 --seed 5 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250 # rasa
+# rm -rf res/
+# python sample.py --image-size 256 --seed 5 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250 #Rasa
 # zip -r DiT-XL-2-256-40000-49999 res/
 
-# python sample.py --image-size 256 --seed 6 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 6 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250 #Anthony
 # zip -r DiT-XL-2-256-50000-549999 res/
 
-# python sample.py --image-size 256 --seed 7 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 7 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-60000-69999 res/
 
-# python sample.py --image-size 256 --seed 8 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 8 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-70000-79999 res/
 
-# python sample.py --image-size 256 --seed 9 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 9 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-80000-89999 res/
 
-# python sample.py --image-size 256 --seed 10 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema -num-sampling-step 250
+# rm -rf res/
+# python sample.py --image-size 256 --seed 10 --n_iter 10 --start_img_id 0 --bs 10 --cfg-scale 1.5 --vae ema --num-sampling-step 250
 # zip -r DiT-XL-2-256-90000-99999 res/
